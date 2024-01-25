717f33e503906bf42c4348094ed7cc74130fb3d5
Salman <engr.sulmannaseer@gmail.com>
2024-01-25 09:36:30 -0500

    create portfolio website in threejs

---
+++ src/constants/index.js
@@ -0,0 +1,224 @@
+import { meta, precast, shandonguni, shopify, starbucks, tesla, tgood, ua, dongshan} from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    cSharp,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    java,
+    python,
+    postgresql,
+    urlshortener,
+    firebase,
+    dotnet
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: postgresql,
+        name: "PostgreSQL",
+        type: "Database",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: firebase,
+        name: "Firebase",
+        type: "Database",
+    },
+];
+
+export const experiences = [
+    {
+        title: "Senior Software Developer",
+        company_name: "Kallidus.",
+        icon: 'https://store-images.s-microsoft.com/image/apps.56959.070c0f59-5335-4607-a91e-407160686dc2.be2ea85a-abb6-4b1d-a389-9d0c4b4cf2b8.881a578e-3c59-4e71-abf8-43665273cdb6.png',
+        iconBg: "white",
+        date: " 2021 - 2024",
+        points: [
+            "Plan, design, develop and maintain Autodesk Revit add-on software applications and plugins.",
+            "Apply Typescript with Nest.js to accomplish OOP and API Programming software solutions using the MVC design pattern.",
+            "Achieve JSON (de)serialization to process Revit design template data storage and retrieval feature implementation.",
+            "Develop and optimize Autodesk Revit add-on tool FrameX for automatic light-frame panelization of walls and floors.",
+            "Resolve 30+ major bugs in FrameX and implement 10+ additional features in FrameX to improve its usability by 15%.",
+            "Design and implement ConcreteX software architecture following OOP principles to handle 95%+ functional requirements.",
+            "Identify and log 10+ extra system requirements, then create corresponding software solutions to upgrade usability.",
+            "Conduct software documentation and coding comment templates using XML comments in project source code to improve 50% code readability.",
+            "Code review over 10+ completed branches and merge into dev and main branches using Git, SourceTree, and BitBucket.",
+            "Utilize Microsoft Word for task solution documentation and Microsoft PowerPoint for project meeting demonstrations.",
+        ],
+        weblink: "https://www.kallidus.com/product/sapling-hris/",
+    },
+    {
+        title: "Software Developer",
+        company_name: "Prisma Technologies",
+        icon: 'http://prismatechcorp.com/wp-content/uploads/2021/07/Logo-PTC-New-Rectangular_fondo-transparente-1.png',
+        iconBg: "white",
+        date: "2020 - 2021",
+        points: [
+            "Participated in Internal Communication Web Application Development on both server and client sides.",
+            "Designed website architecture with user workflow in ERD Diagrams and mock-up user interfaces in Figma.",
+            "Established 30+ Backend REST APIs by setting up schemes, paths, routes, hooks, and server side admin page using Python with Django.",
+            "Generated 100+ user authentication unit testing cases to secure user login and registration features' quality and performance.",
+        ],
+        weblink: "https://prismatechcorp.com/en/home/",
+    },
+    {
+        title: "Software Engineer",
+        company_name: "FutureRx",
+        icon: 'https://trishid.com/images/futureRX_banner.png',
+        iconBg: "white",
+        date: "2019 - 2020",
+        points: [
+            "Participated in Internal Communication Web Application Development on both server and client sides.",
+            "Developed new functionality and features in line with functional specifications and business requirements.",
+            "Developed a user-friendly interface to seamlessly combine the new module with the existing login system by using AngularJS.",
+            "Used the responsive utilities of the Bootstrap frameworks to facilitate the design displaying properly on different devices.",
+            "Designed website architecture with user workflow in ERD Diagrams and mock-up user interfaces in Figma.",
+            "Established 20+ Backend REST APIs by setting up schemes, paths, routes, hooks, and server side admin page using React.js, Node.js, Redis and PostgreSQl.",
+            "Generated 100+ user authentication unit testing cases to secure user login and registration features' quality and performance.",
+        ],
+        weblink: "https://futurerx.com/",
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/salmanNaseer002',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/salman-naseer-dev/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Full Stack Threads Clone',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/MichaelLsh/threads_web_app',
+        weblink: 'https://threads-web-app-phi.vercel.app/',
+    },
+    {
+        iconUrl: 'https://store-images.s-microsoft.com/image/apps.56959.070c0f59-5335-4607-a91e-407160686dc2.be2ea85a-abb6-4b1d-a389-9d0c4b4cf2b8.881a578e-3c59-4e71-abf8-43665273cdb6.png',
+        theme: 'btn-back-blue',
+        name: 'Human Resource Management System',
+        description: 'Automate HR processes with customisable software that works for you. Connect people, tools, and data from onboarding to offboarding and everything in between.',
+        weblink: 'https://www.kallidus.com/product/sapling-hris/',
+    },
+    {
+        iconUrl: 'https://trishid.com/images/futureRX_banner.png',
+        name: 'FutureRx',
+        description: 'The FRx Technology Platform is the foundation for all our services and solutions, designed from the ground up by the most talented innovators in healthcare.',
+        weblink: 'https://futurerx.com/'
+    },
+    {
+        iconUrl: urlshortener,
+        theme: 'btn-back-pink',
+        name: 'URL Shortener Web Application',
+        description: 'Developed a backend web application for users to transfer from long wordy URL links to intuitive ones to store and use.'
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+    },
+];
